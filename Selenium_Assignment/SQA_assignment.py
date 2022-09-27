d0f9920aab4f6571b419c35e72d2681b1e50543f
Shahriar Iqbal <76882473+Shahriar-1810@users.noreply.github.com>
2022-09-27 14:00:29 +0600

    Update SQA_assignment.py

--- Selenium_Assignment/SQA_assignment.py
+++ Selenium_Assignment/SQA_assignment.py
@@ -8,12 +8,12 @@ import time
 
 
 
-#driver = webdriver.Chrome(ChromeDriverManager().install()) #automatically install the current webdriver version, thus we wont need to install again and again.
+#driver = webdriver.Chrome(ChromeDriverManager().install()) #automatically install the current webdriver version, thus we won't need to install again and again.
 
 
 
 #-----------------------------------------------------------------------------------------------------------------------
-#going to the url with the webdriver
+#---------------going to the url with the webdriver--------------
 driver = webdriver.Chrome(executable_path="C:\\Users\\Nafis\\Desktop\\chromedriver_win32\\chromedriver.exe")
 
 
@@ -28,15 +28,15 @@ driver.find_element(By.CLASS_NAME, 'login').click()  #clicking the sign in butto
 #signIn[0].click()                                                        --> line27,28 is another way to click on the sign in button
 
 #-----------------------------------------------------------------------------------------------------------------------
-# #creating the first account
-driver.find_element(By.ID,'email_create').send_keys("nafis.3@hotmail.com") ##line 32--> filling up the email address space of create account section by using its unique id from the inspect
+# ------------------creating the first account-----------------------------
+driver.find_element(By.ID,'email_create').send_keys("nafis.3@hotmail.com") ## filling up the email address field of create account section by using its unique id from the inspect
 
 
 
 driver.find_element(By.ID, 'SubmitCreate').click()  # Easy way to clicking on create account button
 
 
-#in line 40-63--> clicking and filling up the space bars by using their unique id
+#in line 40-63--> clicking and filling up the fields by using their unique id
 driver.find_element(By.ID, 'id_gender1').click()
 driver.find_element(By.ID, 'customer_firstname').send_keys("Nafis")
 driver.find_element(By.ID, 'customer_lastname').send_keys("Iqbal")
@@ -51,7 +51,7 @@ driver.find_element(By.ID, 'optin').click()
 #driver.find_element(By.ID, 'firstname').send_keys("Nafis")
 #driver.find_element(By.ID, 'lastname').send_keys("Iqbal")
 
-driver.find_element(By.ID, 'company').send_keys("BS23")
+driver.find_element(By.ID, 'company').send_keys("Automation_Company")
 driver.find_element(By.ID, 'address1').send_keys("Block-f, Section-2, Dhaka-1216")
 driver.find_element(By.ID, 'address2').send_keys("House-9, Aveneue-1")
 driver.find_element(By.ID, 'city').send_keys("Mirpur")
@@ -70,8 +70,8 @@ driver.find_element(By.CLASS_NAME, 'logout').click() #clicking log out using the
 #time.sleep(5)
 #driver.quit()
 # #------------------------------------------------------------------------------------------------------------------------
-#creating 2nd account
-#in line 75-104--> clicking and filling up the space bars by using their unique id
+#-----------------------creating 2nd account------------------------
+#in line 75-104--> clicking and filling up the fields by using their unique id
 driver.find_element(By.ID,'email_create').send_keys("nafis.4@hotmail.com")
 
 #create = driver.find_elements(By.CSS_SELECTOR, 'div.submit button i')
@@ -92,7 +92,7 @@ driver.find_element(By.ID, 'optin').click()
 #driver.find_element(By.ID, 'firstname').send_keys("Nafis")
 #driver.find_element(By.ID, 'lastname').send_keys("Iqbal")
 
-driver.find_element(By.ID, 'company').send_keys("BS23")
+driver.find_element(By.ID, 'company').send_keys("Manual_company")
 driver.find_element(By.ID, 'address1').send_keys("Block-f, Section-2, Dhaka-1216")
 driver.find_element(By.ID, 'address2').send_keys("House-9, Aveneue-1")
 driver.find_element(By.ID, 'city').send_keys("Mirpur")
@@ -109,13 +109,13 @@ driver.find_element(By.CLASS_NAME, 'logout').click() #clicking log out using the
 
 #-----------------------------------------------------------------------------------------------------------------------
 
-#logging in with the 1st created account from the section 'Already Registered'
+#---------------------------logging in with the 1st created account from the section 'Already Registered'----------------------------------
 driver.find_element(By.ID, 'email').send_keys("nafis.3@hotmail.com")
 driver.find_element(By.ID, 'passwd').send_keys("admin@1234")
 driver.find_element(By.ID, 'SubmitLogin').click()
 
 #-----------------------------------------------------------------------------------------------------------------------
-#From here the task 4 of the pdf file starts and end at task 7 for the 1st user
+#-----------------------------Going to Casual Dresses section and adding a dress into the cart--------------------------------
 
 driver.find_element(By.XPATH, '//*[@id="block_top_menu"]/ul/li[2]/a').click()  #clicking on the DRESSES button using Xpath which can be found from the inspect of that particular portion(DRESSES)
 
@@ -125,7 +125,7 @@ driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[3]/div[2]/ul/li/div
 
 driver.find_element(By.XPATH, '//*[@id="layer_cart"]/div[1]/div[2]/div[4]/span/span/i').click() #clicking the option 'continue shopping by using its xpath
 
-
+#------------------------------Going to the T-shirt section > Filtering the list with blue color > Adding a shirt from the filter list----------------------
 
 driver.find_element(By.XPATH, '/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[3]/a').click()  #clicking on T-Shirt button by usiing its xpath from its inspect
 
@@ -133,6 +133,8 @@ driver.find_element(By.XPATH, '/html/body/div/div[1]/header/div[3]/div/div/div[6
 driver.find_element(By.ID, 'color_2').click() #Filtering the list with blue color by using its unique id 'color_2' which is found from its inspect
 
 driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[3]/div/div/div/div[4]/form/div/div[3]/div[1]/p/button/span').click() # clicking the option 'add to cart' by using its xpath
+
+#--------------------------------------Proceeding to Checkout > Paying by check > Confirming the order > Signing out----------------
 
 driver.find_element(By.XPATH, '/html/body/div/div[1]/header/div[3]/div/div/div[4]/div[1]/div[2]/div[4]/a/span/i').click() #clicking 'proceed to checkout' after adding the product to the cart and the next page will be summary page.  Used xpath again
 
@@ -151,13 +153,13 @@ driver.find_element(By.CLASS_NAME, 'logout').click() #clicking the sign out butt
 
 #------------------------------------------------------------------------------------------------------------------------------------------------
 
-#logging in with the 2nd created account from the section 'Already Registered'
+#---------------------------logging in with the 2nd created account from the section 'Already Registered'-------------------------------------
 driver.find_element(By.ID, 'email').send_keys("nafis.4@hotmail.com")
 driver.find_element(By.ID, 'passwd').send_keys("admin@1234")
 driver.find_element(By.ID, 'SubmitLogin').click()
 
 #-----------------------------------------------------------------------------------------------------------------------
-#From here the task 4 of the pdf file starts and end at task 7 for the 2nd user
+#-----------------------------Going to Casual Dresses section and adding a dress into the cart--------------------------------
 
 driver.find_element(By.XPATH, '//*[@id="block_top_menu"]/ul/li[2]/a').click()  #clicking on the DRESSES button using Xpath which can be found from the inspect of that particular portion(DRESSES)
 
@@ -169,12 +171,18 @@ driver.find_element(By.XPATH, '//*[@id="layer_cart"]/div[1]/div[2]/div[4]/span/s
 # cont = driver.find_elements(By.CSS_SELECTOR, 'div.button-container span i')
 # cont[0].click()                                            #--> another complicated way of clicking the option 'continue shopping'
 
+
+#------------------------------Going to the T-shirt section > Filtering the list with blue color > Adding a shirt from the filter list----------------------
+
 driver.find_element(By.XPATH, '/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[3]/a').click()  #clicking on T-Shirt button by usiing its xpath from its inspect
 
 
 driver.find_element(By.ID, 'color_2').click() #Filtering the list with blue color by using its unique id 'color_2' which is found from its inspect
 
 driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[3]/div/div/div/div[4]/form/div/div[3]/div[1]/p/button/span').click() # clicking the option 'add to cart' by using its xpath
+
+
+#--------------------------------------Proceeding to Checkout > Paying by check > Confirming the order > Signing out----------------
 
 driver.find_element(By.XPATH, '/html/body/div/div[1]/header/div[3]/div/div/div[4]/div[1]/div[2]/div[4]/a/span/i').click() #clicking 'proceed to checkout' after adding the product to the cart and the next page will be summary page.  Used xpath again
 
